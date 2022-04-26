abc4a7a80b8e4f809da1c8b508a3ff75936e3a00
nadduli <naddulidaniel94@gmail.com>
2022-04-26 15:49:49 -0700

    added task 11

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env/python3
+''' task 10 module
+'''
+
+from pymongo import MongoClient
+
+def update_topics(mongo_collection, name, topics):
+    """function to update topics
+    """
+
+    result = mongo_collection.update_many({'name': name}, {$set: {'topics': topics}})
+    return result
