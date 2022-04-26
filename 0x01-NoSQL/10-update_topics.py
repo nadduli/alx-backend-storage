a857828eb9855b2aa904ec7d42624a71c50d7bb7
nadduli <naddulidaniel94@gmail.com>
2022-04-26 15:55:02 -0700

    changes topics based on lessons

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -7,6 +7,8 @@ from pymongo import MongoClient
 def update_topics(mongo_collection, name, topics):
     """function to update topics
     """
-
-    mongo_collection.update_many({'name': name}, {$set: {'topics': topics}})
+    mongo_collection.update_many(
+        {'name': name},
+        {'$set': {'topics': topics}}
+    )
     
