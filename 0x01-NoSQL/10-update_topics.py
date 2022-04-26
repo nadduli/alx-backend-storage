f9fe4d9e20dd017a70762d482baf88c3c7aad835
nadduli <naddulidaniel94@gmail.com>
2022-04-26 16:03:19 -0700

    added task 12

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,14 +1,12 @@
-#!/usr/bin/env/python3
-''' task 10 module
+#!/usr/bin/env python3
+'''Task 10's module.
 '''
 
-from pymongo import MongoClient
 
 def update_topics(mongo_collection, name, topics):
-    """function to update topics
-    """
+    '''Changes all topics of a collection's document based on the name.
+    '''
     mongo_collection.update_many(
         {'name': name},
         {'$set': {'topics': topics}}
     )
-    
